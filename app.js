6183e3e48a4c7ba96c9b122c276f8064ccdb31ca
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-03-26 19:47:06 +0200

    upd

--- app.js
+++ app.js
@@ -10,27 +10,24 @@ const skaiciuoti = () => {
     const kmi = (w / ((h / 100) ** 2)).toFixed(1);
     rez.innerHTML = `KMI: ${kmi}`;
     if (kmi < 18.5) {
-        document.body.style.backgroundColor = "#AED9D1";
-        rez.style.color = "#54949C";
+        document.body.style.backgroundColor = "#AED8D0";
+        rez.style.color = "#AED8D0";
     } else if (kmi >= 18.5 && kmi <= 24.9) {
         document.body.style.backgroundColor = "#9BE756";
-        rez.style.color = "#84C047";
+        rez.style.color = "#9BE756";
     } else if (kmi >= 25 && kmi <= 29.9) {
-        document.body.style.backgroundColor = "#FEFCA5";
-        rez.style.color = "#B4B05A";
-    } else if (kmi >= 30 && kmi <= 34.9) {
         document.body.style.backgroundColor = "#FDD805";
-        rez.style.color = "#B39009";
-    } else if (kmi >= 35 && kmi <= 39.9) {
+        rez.style.color = "#FDD805";
+    } else if (kmi >= 30 && kmi <= 34.9) {
         document.body.style.backgroundColor = "#F68C31";
-        rez.style.color = "#AF4F0A";
-    } else if (kmi >= 40) {
-        document.body.style.backgroundColor = "#F17882";
-        rez.style.color = "#AC4045";
+        rez.style.color = "#F68C31";
+    } else if (kmi >= 35) {
+        document.body.style.backgroundColor = "#F95353";
+        rez.style.color = "#F95353";
     } else {
         document.body.style.backgroundColor = "#faebd7";
         rez.style.color = "#d3d3d3";
-        rez.innerHTML = '???';
+        rez.innerHTML = 'KMI: ???';
     }
 }
 
